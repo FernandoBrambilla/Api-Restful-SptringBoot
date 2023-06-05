0aa30dcb1748a0a23a8ebe44699996f6c2bbf198
FernandoBrambilla <ferbabdobrambillab1@hotmail.com>
2023-06-05 14:39:45 -0300

    first commit

---
+++ src/main/java/com/example/demo/ApiRestfulSptringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSptringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSptringBootApplication.class, args);
+	}
+
+}
