54f55027661ca16d05746a612269297a4e19ce38
FernandoBrambilla <ferbabdobrambillab1@hotmail.com>
2023-06-06 13:34:58 -0300

    DTO return user

--- src/main/java/apiApplication/resources/UserResourses.java
+++ src/main/java/apiApplication/resources/UserResourses.java
@@ -1,6 +1,7 @@
 package apiApplication.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import apiApplication.domain.User;
+import apiApplication.dto.UserDto;
 import apiApplication.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResourses {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDto>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
