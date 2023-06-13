b00eb80bd134cb10d7162959dcd9103402944974
FernandoBrambilla <ferbabdobrambillab1@hotmail.com>
2023-06-13 13:15:30 -0300

    findTitle @Query

--- src/main/java/apiApplication/services/PostService.java
+++ src/main/java/apiApplication/services/PostService.java
@@ -29,8 +29,10 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text){
-		return postRepository.findByTitleContainingIgnoreCase(text);
+		return postRepository.findByTitle(text);
 	}
+	
+	
 	
 	
 
